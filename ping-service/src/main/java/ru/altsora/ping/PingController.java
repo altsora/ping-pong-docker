6594ae885e0f7eadf6cb943984b7769be6fc42ec
avergun <avergun@diasoft.ru>
2022-08-30 22:44:59 +0300

    Настроены Dockerfiles и docker-compose.yml

--- ping-service/src/main/java/ru/altsora/ping/PingController.java
+++ ping-service/src/main/java/ru/altsora/ping/PingController.java
@@ -1,20 +1,26 @@
 package ru.altsora.ping;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+
 @Log4j2
 @RestController
 public class PingController {
-    @Value("${pong-service.url}")
+    @Value("${service.pong.url}")
     private String pongServiceUrl;
 
     private final RestTemplate restTemplate = new RestTemplate();
+
+    @PostConstruct
+    private void init() {
+        log.info("pongServiceUrl = [{}]", pongServiceUrl);
+    }
 
     @GetMapping("/")
     public String whoAmI() {
@@ -24,12 +30,18 @@ public class PingController {
     @GetMapping("/ping")
     public String ping() {
         log.info("call /ping");
+        return "Ping";
+    }
+
+    @GetMapping("/pong")
+    public String callPong() {
+        log.info("call /ping");
         try {
             final String response = restTemplate.getForObject(pongServiceUrl, String.class);
-            return "Ping ... " + response;
+            return "Ping ===> " + response;
         } catch (RestClientException ex) {
             log.error("failed to call pong-service by URL [{}]", pongServiceUrl, ex);
-            return "Ping ...";
+            return "Ping ===>";
         }
     }
 }
--- pong-service/src/main/java/ru/altsora/pong/PongController.java
+++ pong-service/src/main/java/ru/altsora/pong/PongController.java
@@ -1,21 +1,47 @@
 package ru.altsora.pong;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
 
 @Log4j2
 @RestController
 public class PongController {
+    @Value("${service.ping.url}")
+    private String pingServiceUrl;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @PostConstruct
+    private void init() {
+        log.info("pingServiceUrl = [{}]", pingServiceUrl);
+    }
 
     @GetMapping("/")
     public String whoAmI() {
-        return "I am pong service";
+        return "I am pong service!";
     }
 
     @GetMapping("/pong")
     public String pong() {
         log.info("call /pong");
         return "Pong";
+    }
+
+    @GetMapping("/ping")
+    public String callPing() {
+        log.info("call /ping");
+        try {
+            final String response = restTemplate.getForObject(pingServiceUrl, String.class);
+            return "Pong <=== " + response;
+        } catch (RestClientException ex) {
+            log.error("failed to call ping-service by URL [{}]", pingServiceUrl, ex);
+            return "Pong <===";
+        }
     }
 }
