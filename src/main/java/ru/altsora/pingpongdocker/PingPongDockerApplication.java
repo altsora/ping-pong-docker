6b719ac17515387ba8e4a6adabb3f447d5d4c297
avergun <avergun@diasoft.ru>
2022-08-30 20:48:54 +0300

    init commit

---
+++ src/main/java/ru/altsora/pingpongdocker/PingPongDockerApplication.java
@@ -0,0 +1,13 @@
+package ru.altsora.pingpongdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingPongDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingPongDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/altsora/pingpongdocker/PingPongDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.altsora.pingpongdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingPongDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
